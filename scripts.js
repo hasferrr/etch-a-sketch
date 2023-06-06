2fe462e4a0027e511d50f006597d38c1b45a0f4d
hasferrr <79697518+hasferrr@users.noreply.github.com>
2023-06-06 16:37:14 +0700

    implement enableChangeSizeButton

--- scripts.js
+++ scripts.js
@@ -5,12 +5,20 @@ container.style.height = containerWidth + 'px';
 
 let grid;
 let column;
-let color = "orange";
+let color = 'black';
 
 makeGrid(16);
-hoverToFill();
+enableHoverToFill();
+enableChangeSizeButton();
 
 function makeGrid(size) {
+    if (size > 50) {
+        size = 50;
+    } else if (size < 1) {
+        size = 1;
+    }
+    size = Math.floor(size);
+
     grid = document.createElement('div');
     grid.classList.add('grid');
     grid.setAttribute('style', `width:${containerWidth}px; height:${containerWidth / size}px;`)
@@ -26,13 +34,43 @@ function makeGrid(size) {
     for (let i = 0; i < size; i++) {
         container.appendChild(grid.cloneNode(true));
     }
+
+    return size;
 }
 
-function hoverToFill() {
+function removeGrid() {
+    const nodeOfRows = document.querySelectorAll('.grid');
+    nodeOfRows.forEach(row => {
+        container.removeChild(row);
+    });
+}
+
+function enableHoverToFill() {
     const nodeOfDivs = document.querySelectorAll('.column');
     nodeOfDivs.forEach(div => {
         div.addEventListener('mouseover', event => {
             event.target.style.backgroundColor = color;
-        })
-    })
+        });
+    });
+}
+
+function enableChangeSizeButton() {
+    const changeSizeButton = document.querySelector('.change-size');
+    changeSizeButton.addEventListener('click', () => {
+        let size;
+        do {
+            size = prompt("New Size: ");
+            if (size === null) {
+                return;
+            }
+        } while (typeof(size) !== "number" && size === NaN);
+        removeGrid();
+        changeGridText(makeGrid(size)); // makeGrid() is side effect and return the size of grid
+        enableHoverToFill();
+    });
+}
+
+function changeGridText(size) {
+    const gridText = document.querySelector(".grid-text");
+    gridText.textContent = `${size} x ${size} grid`;
 }
